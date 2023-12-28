e364ffa32f9480d56bef2f3dff7ca766b48f407f
Ryan P <93515317+ryan-dxb@users.noreply.github.com>
2023-12-28 10:25:55 +0400

    Added Sorting and filtering by age and gender

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -56,9 +56,19 @@ public class UserRepository : IUserRepository
         var query = context.Users.AsQueryable();
 
         query = query.Where(u => u.UserName != userParams.CurrentUsername);
-
         query = query.Where(u => u.Gender == userParams.Gender);
 
+        // Calculate age
+        var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MaxAge - 1));
+        var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
+
+        query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+
+        query = userParams.OrderBy switch
+        {
+            "created" => query.OrderByDescending(u => u.Created),
+            _ => query.OrderByDescending(u => u.LastActive) // Default to last active
+        };
 
 
         return await PagedList<MemberDto>.CreateAsync(
